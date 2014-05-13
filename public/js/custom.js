2c4a127ad44181fe29e170c357c9ad575d215a1a
Mitchell Cohen <mitch.cohen@me.com>
2014-05-13 04:36:57 -0400

    part 2

--- public/js/custom.js
+++ public/js/custom.js
@@ -26,13 +26,23 @@
 var header_resize = function(){
     var tallness = $(window).innerHeight();
     if (tallness < 600  && tallness > 400) {
+        if (page_skrollr != null) {
+            page_skrollr.destroy();
+        }
         $('.hero-image').css('height', tallness + "px");
     }
     else if (tallness < 400) {
+        if (page_skrollr != null) {
+            page_skrollr.destroy();
+        }
         $('.hero-image').css('height', "400" + "px");
     }
     else {
-        $('.hero-image').css('height', "450" + "px");
+
+        if ('ontouchstart' in document.documentElement == false) {
+            page_skrollr = skrollr.init();
+        }
+        $('.hero-image').css('height', "650" + "px");
     }
 }
 
